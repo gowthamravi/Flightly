5bce2e9b291676794d9afba9bf93460ad72860b6
gowthamravi <gowtham.r3@gmail.com>
2025-12-13 15:00:16 +0530

    temp

--- FlightSearch/BaseURL.swift
+++ FlightSearch/BaseURL.swift
@@ -13,4 +13,5 @@ struct BaseURL {
     static let stationList = #URL("https://mobile-testassets-dev.s3.eu-west-1.amazonaws.com")
     static let flightSearch = #URL("https://nativeapps.ryanair.com/api/v4/Availability")
     static let oneWaySearch = #URL("https://booking-com18.p.rapidapi.com/flights/search-oneway?")
+    
 }
